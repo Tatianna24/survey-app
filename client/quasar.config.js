a511fe2049e578b6ce5262d7dc492446293bd8e4
tati <tatiannar8@nycstudents.net>
2024-02-01 17:44:19 -0500

    fixed prod IP address in quasar.config.sys

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function ( ctx ) {
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://'ssh -o ServerAliveInterval=60 -i ~/Desktop/BAHSWebServer.pem ubuntu@3.84.235.6
+        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://3.84.235.6',
         CREATOR_EMAIL: 'Tatiannar0115@gmail.com',
         LINKEDIN: 'https://www.linkedin.com/in//tatiannarodriguez'
       },
